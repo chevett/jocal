7a1a11905e6722d1a0e5747e9a8444e54bcd5d15
Mike Chevett <mchevett@gmail.com>
2014-08-13 11:41:05 -0400

    just pretend it's a browser when it isn't.  for testing.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+var fake = {},
+	IS_BROWSER = typeof window !== 'undefined';
+
 function _localStorage(key, value){
 	var returnVal;
 
 	if (value === undefined){
 		try {
-			returnVal = window.localStorage.getItem(key);
-			returnVal = JSON.parse(returnVal);
+			if (IS_BROWSER){
+				returnVal = window.localStorage.getItem(key);
+				returnVal = JSON.parse(returnVal);
+			} else {
+				returnVal = JSON.parse(fake[key]);
+			}
 		}
 		catch(err){
 			console.error(err);
@@ -14,7 +21,12 @@ function _localStorage(key, value){
 		}
 	}
 
-	window.localStorage.setItem(key, JSON.stringify(value));
+	if (IS_BROWSER){
+		window.localStorage.setItem(key, JSON.stringify(value));
+	} else {
+		fake[key] = JSON.stringify(value);
+	}
+
 	return value;
 }
 
